8d9f57b7257b712a599c7d276e2507d364c6d75a
gautamamber <ambergautam1@gmail.com>
2019-10-02 16:28:38 +0530

    feat():Add cognito pool id and name in constant

---
+++ constant.py
@@ -0,0 +1,9 @@
+class Constants:
+    """Constans like:
+        Cognito pool Id
+        Cognito client Id
+        DynamoTable name
+        Messages
+    """
+    COGNITO_POOL_ID = "us-west-2_DsgIuVhGV"
+    COGNITO_CLIENT_ID = "4gffc9o9kkgovvlcclpasqeab0"
